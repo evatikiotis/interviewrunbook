c19a82320b6ffda1f56f9b2bfc93183cad9df213
Evangelos Vatikiotis <evangelos.vatikiotis@capgemini.com>
2024-09-20 15:00:45 +0200

    docusaurus based pipelie

--- build/assets/js/5e95c892.60923d7f.js
+++
@@ -1,1 +0,0 @@
-"use strict";(self.webpackChunkinterviewrunbook=self.webpackChunkinterviewrunbook||[]).push([[9647],{7121:(e,r,s)=>{s.r(r),s.d(r,{default:()=>o});s(6540);var n=s(4164),u=s(1213),t=s(7559),a=s(2831),c=s(781),i=s(4848);function o(e){return(0,i.jsx)(u.e3,{className:(0,n.A)(t.G.wrapper.docsPages),children:(0,i.jsx)(c.A,{children:(0,a.v)(e.route.routes)})})}}}]);
--- build/assets/js/acecf23e.074c357e.js
+++
@@ -1,1 +0,0 @@
-"use strict";(self.webpackChunkinterviewrunbook=self.webpackChunkinterviewrunbook||[]).push([[1903],{1912:e=>{e.exports=JSON.parse('{"blogBasePath":"/interviewrunbook/blog","blogTitle":"Blog","authorsListPath":"/interviewrunbook/blog/authors"}')}}]);
